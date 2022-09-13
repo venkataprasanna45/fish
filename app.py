e82e35c0831cabaf88bef0dc130b7dcf914336b2
kallasrikanth1999 <kalla.srikanth010@gmail.com>
2022-09-13 15:12:32 +0530

    fishweight
    fishing

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,url_for,request,redirect
+import numpy as np
+import pandas as pd
+import joblib
+import pickle
+
+
+app = Flask(__name__)
+
+model = joblib.load('Random.pkl')
+onehot = joblib.load('one_joblib')
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_features =[[x for x in request.form.values()]]
+	print("$"*30)
+	print(int_features)
+	c = ['Species','Length1', 'Length2', 'Length3', 'Height','Width']
+	df = pd.DataFrame(int_features,columns=c)
+	l = onehot.transform(df.iloc[:,:1])
+	c = onehot.get_feature_names_out()
+	t = pd.DataFrame(l,columns=c)
+	l2 = df.iloc[:,1:]
+	final =pd.concat([t,l2],axis=1)
+	result = model.predict(final)
+	print("The Result is :",result)
+
+
+	print(int_features)
+
+	return render_template("main.html",prediction_text=" The estimated fish weight is {} in gms.".format(result))
+
+
+if __name__ == "__main__":
+	app.debug=True
+	app.run(host = '127.0.0.1',port=8000)
